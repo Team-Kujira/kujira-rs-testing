100fac9ce1a66d3a035c1faa00d5ef859b33b8a8
codehans <94654388+codehans@users.noreply.github.com>
2022-09-23 12:23:33 +0100

    handle reverse FIN swaps

--- src/fin.rs
+++ src/fin.rs
@@ -4,14 +4,13 @@ use std::convert::TryFrom;
 use cosmwasm_std::entry_point;
 
 use cosmwasm_std::{
-    coins, BankMsg, Binary, CosmosMsg, Decimal256, Deps, DepsMut, Env, MessageInfo, Response,
-    StdResult, Uint128, Uint256,
+    coins, BankMsg, Binary, CosmosMsg, Decimal256, Deps, DepsMut, Env, Fraction, MessageInfo,
+    Response, StdError, StdResult, Uint128, Uint256,
 };
 use kujira::{
     fin::{ExecuteMsg, InstantiateMsg, QueryMsg},
     msg::KujiraMsg,
     query::KujiraQuery,
-    utils::amount,
 };
 
 const STABLE: &str = "factory/contract0/uusk";
@@ -37,14 +36,25 @@ pub fn execute(
     let sender = info.sender.clone();
     match msg {
         ExecuteMsg::Swap { belief_price, .. } => {
-            let amount: Uint256 = amount(&STABLE.into(), info.funds)?.into();
-            let price = belief_price.unwrap_or_else(|| Decimal256::from_ratio(100u128, 1425u128));
+            let coin = info.funds[0].clone();
+            let amount: Uint256 = coin.amount.into();
+            let price = belief_price.unwrap_or_else(|| Decimal256::from_ratio(1425u128, 100u128));
+
+            let (price, return_denom) = match coin.denom.as_str() {
+                STABLE => (
+                    Decimal256::from_ratio(price.denominator(), price.numerator()),
+                    COLLATERAL,
+                ),
+                COLLATERAL => (price, STABLE),
+                _ => return Err(StdError::generic_err("Invalid Denom")),
+            };
+
             let return_amount: Uint128 = Uint128::try_from(amount * price)?;
 
             Ok(
                 Response::default().add_messages(vec![CosmosMsg::Bank(BankMsg::Send {
                     to_address: sender.to_string(),
-                    amount: coins(return_amount.u128(), COLLATERAL.to_string()),
+                    amount: coins(return_amount.u128(), return_denom),
                 })]),
             )
         }
