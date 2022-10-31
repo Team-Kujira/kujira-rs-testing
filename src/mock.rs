0202fee3133f47bb2639556fd4a9a7eba4f880d5
codehans <94654388+codehans@users.noreply.github.com>
2022-10-31 14:07:31 +0000

    Better testing for DenomMsg

--- src/mock.rs
+++ src/mock.rs
@@ -1,13 +1,15 @@
+use std::convert::TryInto;
+
 use anyhow::Result as AnyResult;
 use cosmwasm_std::{
     attr,
     testing::{MockApi, MockStorage},
-    to_binary, Addr, Coin, Decimal, Empty, Event, Uint128,
+    to_binary, Addr, BankMsg, Coin, CosmosMsg, Decimal, Empty, Event, Uint128,
 };
 
 use cw_multi_test::{
-    App, AppResponse, BankKeeper, BasicAppBuilder, CosmosRouter, FailingDistribution,
-    FailingStaking, Module, WasmKeeper,
+    App, AppResponse, BankKeeper, BankSudo, BasicAppBuilder, CosmosRouter, FailingDistribution,
+    FailingStaking, Module, SudoMsg, WasmKeeper,
 };
 
 use kujira::{
@@ -28,7 +30,6 @@ pub type CustomApp = App<
 pub fn mock_app(balances: Vec<(Addr, Vec<Coin>)>) -> CustomApp {
     let custom = KujiraModule {
         oracle_price: Decimal::from_ratio(1425u128, 100u128),
-        supply: Uint128::zero(),
     };
     BasicAppBuilder::new_custom()
         .with_custom(custom)
@@ -41,16 +42,11 @@ pub fn mock_app(balances: Vec<(Addr, Vec<Coin>)>) -> CustomApp {
 
 pub struct KujiraModule {
     pub oracle_price: Decimal,
-    pub supply: Uint128,
 }
 
 impl KujiraModule {
     pub fn set_oracle_price(&mut self, price: Decimal) {
         self.oracle_price = price;
-    }
-
-    pub fn set_supply(&mut self, supply: Uint128) {
-        self.supply = supply;
     }
 }
 
@@ -63,11 +59,11 @@ impl Module for KujiraModule {
 
     fn execute<ExecC, QueryC>(
         &self,
-        _api: &dyn cosmwasm_std::Api,
-        _storage: &mut dyn cosmwasm_std::Storage,
-        _router: &dyn CosmosRouter<ExecC = ExecC, QueryC = QueryC>,
-        _block: &cosmwasm_std::BlockInfo,
-        _sender: Addr,
+        api: &dyn cosmwasm_std::Api,
+        storage: &mut dyn cosmwasm_std::Storage,
+        router: &dyn CosmosRouter<ExecC = ExecC, QueryC = QueryC>,
+        block: &cosmwasm_std::BlockInfo,
+        sender: Addr,
         msg: Self::ExecT,
     ) -> AnyResult<AppResponse>
     where
@@ -82,29 +78,73 @@ impl Module for KujiraModule {
         match msg {
             KujiraMsg::Auth(_) => todo!(),
             KujiraMsg::Denom(d) => match d {
-                DenomMsg::Create { .. } => Ok(AppResponse {
-                    events: vec![],
-                    data: None,
-                }),
+                DenomMsg::Create { subdenom } => {
+                    storage.set(subdenom.as_bytes(), &Uint128::zero().to_be_bytes());
+
+                    Ok(AppResponse {
+                        events: vec![],
+                        data: None,
+                    })
+                }
                 DenomMsg::Mint {
                     amount,
                     denom,
                     recipient,
-                } => Ok(AppResponse {
-                    events: vec![Event::new("mint").add_attributes(vec![
-                        attr("amount", amount),
-                        attr("denom", denom.to_string()),
-                        attr("recipient", recipient),
-                    ])],
-                    data: None,
-                }),
-                DenomMsg::Burn { denom, amount } => Ok(AppResponse {
-                    events: vec![Event::new("burn").add_attributes(vec![
-                        attr("amount", amount),
-                        attr("denom", denom.to_string()),
-                    ])],
-                    data: None,
-                }),
+                } => {
+                    let mut supply = storage
+                        .get(denom.as_bytes())
+                        .map(|bz| u128::from_be_bytes(bz.try_into().unwrap()))
+                        .map(Uint128::from)
+                        .unwrap_or_default();
+
+                    supply += amount;
+                    storage.set(denom.as_bytes(), &Uint128::from(supply).to_be_bytes());
+                    router.sudo(
+                        api,
+                        storage,
+                        block,
+                        SudoMsg::Bank(BankSudo::Mint {
+                            to_address: recipient.to_string(),
+                            amount: denom.coins(&amount),
+                        }),
+                    )?;
+                    Ok(AppResponse {
+                        events: vec![Event::new("mint").add_attributes(vec![
+                            attr("amount", amount),
+                            attr("denom", denom.to_string()),
+                            attr("recipient", recipient),
+                        ])],
+                        data: None,
+                    })
+                }
+                DenomMsg::Burn { denom, amount } => {
+                    let mut supply = storage
+                        .get(denom.as_bytes())
+                        .map(|bz| u128::from_be_bytes(bz.try_into().unwrap()))
+                        .map(Uint128::from)
+                        .unwrap_or_default();
+
+                    supply -= amount;
+                    storage.set(denom.as_bytes(), &Uint128::from(supply).to_be_bytes());
+
+                    router.execute(
+                        api,
+                        storage,
+                        block,
+                        sender,
+                        CosmosMsg::Bank(BankMsg::Burn {
+                            amount: denom.coins(&amount),
+                        }),
+                    )?;
+
+                    Ok(AppResponse {
+                        events: vec![Event::new("burn").add_attributes(vec![
+                            attr("amount", amount),
+                            attr("denom", denom.to_string()),
+                        ])],
+                        data: None,
+                    })
+                }
                 _ => todo!(),
             },
         }
@@ -133,16 +173,23 @@ impl Module for KujiraModule {
     fn query(
         &self,
         _api: &dyn cosmwasm_std::Api,
-        _storage: &dyn cosmwasm_std::Storage,
+        storage: &dyn cosmwasm_std::Storage,
         _querier: &dyn cosmwasm_std::Querier,
         _block: &cosmwasm_std::BlockInfo,
         request: Self::QueryT,
     ) -> AnyResult<cosmwasm_std::Binary> {
         match request {
             KujiraQuery::Bank(b) => match b {
-                BankQuery::Supply { denom } => Ok(to_binary(&SupplyResponse {
-                    amount: denom.coin(&self.supply),
-                })?),
+                BankQuery::Supply { denom } => {
+                    let supply = storage
+                        .get(denom.as_bytes())
+                        .map(|bz| u128::from_be_bytes(bz.try_into().unwrap()))
+                        .unwrap_or_default();
+
+                    Ok(to_binary(&SupplyResponse {
+                        amount: denom.coin(&Uint128::from(supply)),
+                    })?)
+                }
             },
             KujiraQuery::Oracle(o) => match o {
                 OracleQuery::ExchangeRate { .. } => Ok(to_binary(&ExchangeRateResponse {
